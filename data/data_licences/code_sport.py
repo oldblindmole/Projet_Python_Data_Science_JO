dea656c267438117e3871fcb9b5c04b21b358269
romanepluquet <romane.pluquet@yahoo.fr>
2025-11-07 18:52:00 +0100

    code_sport

---
+++ data/data_licences/code_sport.py
@@ -0,0 +1,54 @@
+"""Ajout d'une variable Code_sport au data frame"""
+
+import pandas as pd
+
+df = pd.read_parquet("licences_long.parquet")
+
+fed_list = df["Fédération"].unique()
+code_list = ["ATH", "AVI", "BAD", "BAK",
+             "BOX", "CAK", "CYC", "EQU", 
+             "ESC", "FOO", "DIV", "GYM", 
+             "HAL", "HAN", "HOC", "JUD", 
+             "LUT", "NAT", "PEN", "DIV", 
+             "TAE", "TEN", "TDT", "TIR", 
+             "TAR", "TRI", "VOI", "VOL", 
+             "DIV", "DIV", "DIV", "ESD", 
+             "SKT", "SUR", "BAS", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "KAR", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV", "DIV", "DIV", 
+             "DIV", "DIV"]
+
+cs = pd.DataFrame({
+    "Fédération": fed_list,
+    "Code_sport": code_list
+    })
+
+# harmonise l'apostrophe entre les deux data frame
+cs["Fédération"] = cs["Fédération"].str.replace("’", "'", regex=False)
+
+df = df.merge(
+  cs,
+  left_on = ["Fédération"],
+  right_on = ["Fédération"],
+  how = "left"
+)
+
+
