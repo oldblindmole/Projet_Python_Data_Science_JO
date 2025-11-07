2f66031f3c0f907e21236370ad618e894170a1ec
oldblindmole <camille.peythieux@posteo.net>
2025-11-07 12:01:02 +0100

    table medaille merge & simplifiée

--- data/data_medailles/table_complete.py
+++ data/data_medailles/table_complete.py
@@ -2,6 +2,10 @@
 
 import pandas as pd
 import numpy as np
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 data_or = pd.read_csv("data_or.csv")
 data_argent = pd.read_csv("data_argent.csv")
@@ -9,22 +13,21 @@ data_bronze = pd.read_csv("data_bronze.csv")
 
 diff_medailles = [data_or, data_argent, data_bronze]
 
+# enlever les lignes vides / qui ne nous intéressent pas
 for i in range(len(diff_medailles)):
     diff_medailles[i] = diff_medailles[i].dropna(how="all")
     diff_medailles[i] = diff_medailles[i].drop(diff_medailles[i].tail(3).index)
-    diff_medailles = [df[["Sport", "2024", "2020"]] for df in diff_medailles]
 
+# enlever les colonnes (années notamment) qui ne nous intéressent pas
+diff_medailles = [df[["Sport", "2024", "2020"]] for df in diff_medailles]
+
+# renommer les variables pour préparer le merge
 diff_medailles[0].columns = ["Sport", "2024_or", "2020_or"]
 diff_medailles[1].columns = ["Sport", "2024_argent", "2020_argent"]
 diff_medailles[2].columns = ["Sport", "2024_bronze", "2020_bronze"]
 
+# merge
 data_medailles = diff_medailles[0].merge(diff_medailles[1], on="Sport", how="outer")
 data_medailles = data_medailles.merge(diff_medailles[2], on="Sport", how="outer")
 
 print(data_medailles)
-
-# enlever les lignes vides
-
-# enlever les années qui ne nous intéressent pas
-
-# merge par sport
