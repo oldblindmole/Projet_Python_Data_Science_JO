3c19be8ab6c46e17dcace4f98a141502e5c525b8
oldblindmole <camille.peythieux@posteo.net>
2025-11-30 09:02:45 +0100

    test medailles cde cdm

---
+++ data/data_medailles/tables_medailles_cdm.py
@@ -0,0 +1,12 @@
+"""Fichier créant un csv d'une base de données répertoriant les médailles gagnées
+pendant les coupes du monde par sport"""
+
+import pandas as pd
+
+data_medailles_cdm = pd.DataFrame({
+    "code_sport": ["RUG", "HAN", "TDT", "NAT", "JUD"],
+    "sport": ["Rugby", "Handball", "Tennis de table", "Natation", "Judo"],
+    #TODO
+})
+
+#TODO rajouter colonne de total
