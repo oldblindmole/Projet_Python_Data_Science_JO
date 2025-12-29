eed6b121345aee6189b386ca147e34f6fc43bee9
Melissamgn <melissa.migan@ensae.fr>
2025-12-29 20:31:40 +0100

    init

--- model/__init__.py
+++ model/__init__.py
@@ -4,5 +4,5 @@ Outils de modélisation des licenciés sportifs.
 Ce package regroupe les briques nécessaires pour :
 1) construire un dataset sport–année enrichi (feature engineering),
 2) estimer un modèle économétrique (effet des médailles sur la croissance),
-3) entraîner un modèle prédictif (Ridge) et visualiser des prédictions.
+3) appuyer les resultats avec des analyses économétriques plus approfondies.
 """
